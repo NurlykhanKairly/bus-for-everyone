c087582e3ca3b71f83fd0e8e93030858a51364f6
Kaiyrly <kayrasktl@gmail.com>
2021-05-22 05:56:31 +0900

    Bus list implemention

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import BusListScreen from './BusListScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const navigator = createStackNavigator(
+  {
+    BusList: BusListScreen
+  }, {
+  initialRouteName: 'BusList',
+  defaultNavigationOptions: {
+    title: 'bus for everyone'
+  }
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator);
